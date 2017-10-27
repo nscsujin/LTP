0523ef99581fd8df7fe17ba2786643baaf112088
Sunny Jin <sunny.jin@flex.com>
2017-10-26 23:29:53 -0700

    [Extension] Rename screen model "loginclassic" to "login"

--- client/mocks/login.js
+++ client/mocks/login.js
@@ -1,7 +1,7 @@
 'use strict';
 window.mocks = window.mocks || {};
-window.mocks.loginclassic = window.mocks.loginclassic || {};
-window.mocks.loginclassic.submit = function ($scope, params, app) {
+window.mocks.login = window.mocks.login || {};
+window.mocks.login.submit = function ($scope, params, app) {
     if (window.plugins && window.plugins.touchid) {
         window.plugins.touchid.save('credentials', JSON.stringify(params.data), function () {
             alert('logged in!');
