2046af25c5006eceffe469570db294bf93772ce8
Sunny Jin <sunny.jin@flex.com>
2017-10-26 23:40:41 -0700

    [Studio] Add screen "Home-Cool"

--- client/states.js
+++ client/states.js
@@ -42,5 +42,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.homecool', {
+        views: {
+            app: {
+                controller: 'app_homecool',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.homecool');
+                }
+            }
+        }
     });
 });
